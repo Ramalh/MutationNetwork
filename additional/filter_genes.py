78fe29103fdf66072fec4e1a964c3bf32ac26303
ramalh <hramal45@gmail.com>
2025-02-03 17:04:31 +0300

    updating mutation file and filter_genes.py script

--- additional/filter_genes.py
+++ additional/filter_genes.py
@@ -13,7 +13,7 @@ if len(gene_types) == 1:
 	data1.to_csv(f"genes_{gene_types[0]}.csv", index=False)
 	sys.exit(0)
 
-for gene_type in gene_types:
+for gene_type in gene_types[1:]:
 	data2 = data[["Source", "Chromosome", "Start", "End", "gene_id", "gene_name", "gene_type", "hgnc_id"]][(data.Feature=="gene") & (data.gene_type==gene_type)].df
 	data1 = pd.concat([data1, data2])
 
